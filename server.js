26bf54a9d3c559b25467fba6852c75ce43a22a48
gauravchaulagain <gauravchaulagain45@gmail.com>
2022-01-20 21:19:35 +0545

    user model

--- server.js
+++ server.js
@@ -11,12 +11,8 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
-
-app.get('/', (req, res) => {
-    res.json({msg: 'Hello world!'})
-
-
-})
+//Routes require
+app.use('/api', require('./routes/authRouter'))
 
 const URI = process.env.MONGODB_URL
 mongoose.connect(URI,{
